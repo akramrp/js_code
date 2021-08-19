ccf788ef469d449900335af83d4aebe2b1093e67
akramrp <akramrp7@gmail.com>
2021-08-19 12:01:30 +0530

    add js interview file

---
+++ interview_js.js
@@ -0,0 +1,14 @@
+/*====================================== interview question and answer ======================================*/
+function hello(){
+	return 'hello world!';
+}
+var res1 = hello();
+console.log(res1);
+
+
+
+
+
+
+
+/*====================================== interview question and answer ======================================*/
