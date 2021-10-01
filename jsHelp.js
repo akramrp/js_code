5cf6f90fcde2f2a05d6ac8851471eb0f59e8e8f3
akramrp <akramrp7@gmail.com>
2021-10-01 11:38:52 +0530

    changes and add files

---
+++ jsHelp.js
@@ -0,0 +1,26 @@
+/*
+check node version:
+    node -v
+    npm -v
+
+install react from terminal:
+    a. npm -g create-react-app
+    b. create-react-app -version
+    c. create-react-app --version
+    d. create-react-app <projectName>
+
+after inatall react app with projectName:
+    1. npm start
+    2. npm run build
+    3. npm test
+    4. npm run eject
+    5. npm fund
+    6. npm audit
+    7. npm audit fix
+
+
+
+
+
+
+*/
